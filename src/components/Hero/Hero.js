3c837bb760e42cd4776cd2af946d19b9421e24fd
Brian Chebon <brynnlamura@gmail.com>
2023-10-30 18:19:46 +0300

    add Navbar and Hero with style

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css"
+
+const Hero = () => {
+    return (
+        <div className="Hero">
+            <h1>Looking for Music</h1>
+            <p>Start Listening Now.</p>
+        </div>
+
+    )
+}
+
+export default Hero;
